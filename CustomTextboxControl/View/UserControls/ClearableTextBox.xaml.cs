ccebb4fce3b3ca6de8d06ef620d78ff5d4737464
SharlottaL <botanichkagigantika@gmail.com>
2025-11-28 10:12:43 +0300

    Enter DONE

--- CustomTextboxControl/View/UserControls/ClearableTextBox.xaml.cs
+++ CustomTextboxControl/View/UserControls/ClearableTextBox.xaml.cs
@@ -49,11 +49,10 @@ namespace CustomTextboxControl.View.UserControls
 
         private void txtInput_KeyDown(object sender, KeyEventArgs e)
         {
-            TraversalRequest request = new TraversalRequest(FocusNavigationDirection.Down);
-            if (e.Key == Key.Enter)
-            ///https://learn.microsoft.com/en-us/dotnet/desktop/wpf/advanced/focus-overview#:~:text=navigationMenu%2C%0A%20%20%20%20KeyboardNavigationMode.Cycle)%3B-,%D0%9F%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%BD%D0%BE%D0%B5%20%D0%BF%D0%B5%D1%80%D0%B5%D0%BC%D0%B5%D1%89%D0%B5%D0%BD%D0%B8%D0%B5%20%D1%84%D0%BE%D0%BA%D1%83%D1%81%D0%B0,-%D0%94%D0%BE%D0%BF%D0%BE%D0%BB%D0%BD%D0%B8%D1%82%D0%B5%D0%BB%D1%8C%D0%BD%D1%8B%D0%BC%D0%B8%20API%20%D0%B4%D0%BB%D1%8F
+            if (e.Key == Key.Enter && txtInput != null && !string.IsNullOrWhiteSpace(txtInput.Text))//txtInput.Text.Length > 0)
+            ///https://learn.microsoft.com/en-us/dotnet/desktop/wpf/advanced/focus-overview#:~:text=navigationMenu%2C%0A%20%20%20%20KeyboardNavigationMode.Cycle)%3B-,%D0%9F%D1%80%D0%BE%D0%B3%D1%80%D0%B0%D0%BC%D0%BC%D0%BD%D0%BE%D0%B5%20%D0%BF%D0%B5%D1%80%D0%B5%D0%BC%D0%B5%D1%89%D0%B5%D0%BD%D0%B8%D0%B5%20%D1%84%D0%BE%D0%BA%D1%83%D1%81%D0%B0,%D0%92%20%D1%81%D0%BB%D0%B5%D0%B4%D1%83%D1%8E%D1%89%D0%B5%D0%BC%20%D0%BF%D1%80%D0%B8%D0%BC%D0%B5%D1%80%D0%B5%20MoveFocus%20%D0%B8%D1%81%D0%BF%D0%BE%D0%BB%D1%8C%D0%B7%D1%83%D0%B5%D1%82%D1%81%D1%8F%20%D0%B4%D0%BB%D1%8F%20%D0%B8%D0%B7%D0%BC%D0%B5%D0%BD%D0%B5%D0%BD%D0%B8%D1%8F%20%D1%81%D1%84%D0%BE%D0%BA%D1%83%D1%81%D0%B8%D1%80%D0%BE%D0%B2%D0%B0%D0%BD%D0%BD%D0%BE%D0%B3%D0%BE%20%D1%8D%D0%BB%D0%B5%D0%BC%D0%B5%D0%BD%D1%82%D0%B0.,-%D0%A1%23
             {
-                txtInput.MoveFocus(request);
+                txtInput.MoveFocus(new TraversalRequest(FocusNavigationDirection.Down));
                 e.Handled = true;
             }
                 
